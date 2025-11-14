e9b23d123125e2f4a14932c36236b39ea944e77e
cheezi0747 <linus.sjolinder@b3.se>
2025-11-14 10:23:51 +0100

    HYDRAN-704: use correct ID when getting singinginformation from DRL

--- src/main/java/se/sundsvall/postportalservice/service/HistoryService.java
+++ src/main/java/se/sundsvall/postportalservice/service/HistoryService.java
@@ -3,7 +3,9 @@ package se.sundsvall.postportalservice.service;
 import static org.zalando.problem.Status.NOT_FOUND;
 import static se.sundsvall.postportalservice.integration.db.converter.MessageType.DIGITAL_REGISTERED_LETTER;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,7 @@ import se.sundsvall.postportalservice.api.model.Message;
 import se.sundsvall.postportalservice.api.model.MessageDetails;
 import se.sundsvall.postportalservice.api.model.Messages;
 import se.sundsvall.postportalservice.api.model.SigningInformation;
+import se.sundsvall.postportalservice.integration.db.MessageEntity;
 import se.sundsvall.postportalservice.integration.db.dao.MessageRepository;
 import se.sundsvall.postportalservice.integration.digitalregisteredletter.DigitalRegisteredLetterIntegration;
 import se.sundsvall.postportalservice.service.mapper.HistoryMapper;
@@ -37,26 +40,41 @@ public class HistoryService {
 		final var page = messageRepository.findAllByMunicipalityIdAndUserUsernameIgnoreCase(municipalityId, username, pageable);
 		final var messages = historyMapper.toMessageList(page.getContent());
 
-		decorateWithSigningInformation(municipalityId, messages);
+		decorateWithSigningInformation(municipalityId, page.getContent(), messages);
 
 		return Messages.create()
 			.withMetaData(PagingAndSortingMetaData.create().withPageData(page))
 			.withMessages(messages);
 	}
 
-	private void decorateWithSigningInformation(final String municipalityId, final List<Message> messages) {
-		// Filter out ids for all correspondence that has been sent as digital registered letters
-		final var digitalRegisteredLetterIds = messages.stream()
-			.filter(message -> DIGITAL_REGISTERED_LETTER.name().equals(message.getType()))
-			.map(Message::getMessageId)
-			.toList();
+	private void decorateWithSigningInformation(final String municipalityId, final List<MessageEntity> messageEntities, final List<Message> messages) {
+		// Create a map of the letterId-> messageId for digital registered letters
+		final Map<String, String> letterIdToMessageIdMap = new HashMap<>();
+		messageEntities.stream()
+			.filter(entity -> DIGITAL_REGISTERED_LETTER.equals(entity.getMessageType()))
+			.forEach(entity -> {
+				final var letterId = entity.getRecipients().getFirst().getExternalId();
+				if (letterId != null) {
+					letterIdToMessageIdMap.put(letterId, entity.getId());
+				}
+			});
 
-		// Fetch signing status for the ids that has been found
-		digitalRegisteredLetterIntegration.getLetterStatuses(municipalityId, digitalRegisteredLetterIds).stream()
-			.forEach(letterStatus -> messages.stream()
-				.filter(message -> message.getMessageId().equals(letterStatus.getLetterId()))
-				.findFirst()
-				.ifPresent(message -> message.setSigningStatus(historyMapper.toSigningStatus(letterStatus))));
+		if (letterIdToMessageIdMap.isEmpty()) {
+			return;
+		}
+
+		// Fetch signing status for the letter IDs that have been found
+		final var letterIds = letterIdToMessageIdMap.keySet().stream().toList();
+		digitalRegisteredLetterIntegration.getLetterStatuses(municipalityId, letterIds)
+			.forEach(letterStatus -> {
+				final var messageId = letterIdToMessageIdMap.get(letterStatus.getLetterId());
+				if (messageId != null) {
+					messages.stream()
+						.filter(message -> message.getMessageId().equals(messageId))
+						.findFirst()
+						.ifPresent(message -> message.setSigningStatus(historyMapper.toSigningStatus(letterStatus)));
+				}
+			});
 	}
 
 	public MessageDetails getMessageDetails(final String municipalityId, final String username, final String messageId) {
@@ -67,9 +85,10 @@ public class HistoryService {
 
 		// Decorate with signing information if this is a digital registered letter
 		if (DIGITAL_REGISTERED_LETTER.equals(messageEntity.getMessageType())) {
-			final var letterStatuses = digitalRegisteredLetterIntegration.getLetterStatuses(municipalityId, List.of(messageId));
+			final var letterId = getLetterIdFromMessage(messageEntity);
+			final var letterStatuses = digitalRegisteredLetterIntegration.getLetterStatuses(municipalityId, List.of(letterId));
 			letterStatuses.stream()
-				.filter(letterStatus -> messageId.equals(letterStatus.getLetterId()))
+				.filter(letterStatus -> letterId.equals(letterStatus.getLetterId()))
 				.findFirst()
 				.ifPresent(letterStatus -> messageDetails.setSigningStatus(historyMapper.toSigningStatus(letterStatus)));
 		}
@@ -78,23 +97,27 @@ public class HistoryService {
 	}
 
 	public SigningInformation getSigningInformation(final String municipalityId, final String messageId) {
-		final var message = messageRepository.findByIdAndMessageType(messageId, DIGITAL_REGISTERED_LETTER)
-			.orElseThrow(() -> Problem.valueOf(NOT_FOUND, "No digital registered letter found for id '%s'".formatted(messageId)));
+		final var message = getDigitalRegisteredLetterMessage(messageId);
+		final var letterId = getLetterIdFromMessage(message);
 
-		// Digital registered letters are always sent to a single recipient
-		final var externalId = message.getRecipients().getFirst().getExternalId();
-
-		return digitalRegisteredLetterIntegration.getSigningInformation(municipalityId, externalId);
+		return digitalRegisteredLetterIntegration.getSigningInformation(municipalityId, letterId);
 	}
 
 	public ResponseEntity<StreamingResponseBody> getLetterReceipt(final String municipalityId, final String messageId) {
-		final var message = messageRepository.findByIdAndMessageType(messageId, DIGITAL_REGISTERED_LETTER)
+		final var message = getDigitalRegisteredLetterMessage(messageId);
+		final var letterId = getLetterIdFromMessage(message);
+
+		return digitalRegisteredLetterIntegration.getLetterReceipt(municipalityId, letterId);
+	}
+
+	private MessageEntity getDigitalRegisteredLetterMessage(final String messageId) {
+		return messageRepository.findByIdAndMessageType(messageId, DIGITAL_REGISTERED_LETTER)
 			.orElseThrow(() -> Problem.valueOf(NOT_FOUND, "No digital registered letter found for id '%s'".formatted(messageId)));
+	}
 
+	private String getLetterIdFromMessage(final MessageEntity message) {
 		// Digital registered letters are always sent to a single recipient
-		final var externalId = message.getRecipients().getFirst().getExternalId();
-
-		return digitalRegisteredLetterIntegration.getLetterReceipt(municipalityId, externalId);
+		return message.getRecipients().getFirst().getExternalId();
 	}
 
 }
