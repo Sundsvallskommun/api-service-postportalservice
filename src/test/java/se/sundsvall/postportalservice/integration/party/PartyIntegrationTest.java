15f2caee82555dc65d464b65ec48b8d5e0b2c4f1
AndreSoderlund <andre.soderlund@b3.se>
2026-01-19 11:08:18 +0100

    Party is not returning failed legalIds in the getPartyId & getLegalId, added logic to loop through the response to find any rejected legalIds.

--- src/test/java/se/sundsvall/postportalservice/integration/party/PartyIntegrationTest.java
+++ src/test/java/se/sundsvall/postportalservice/integration/party/PartyIntegrationTest.java
@@ -9,7 +9,6 @@ import static org.zalando.problem.Status.BAD_GATEWAY;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,7 +21,6 @@ import org.zalando.problem.Problem;
 class PartyIntegrationTest {
 
 	private static final String MUNICIPALITY_ID = "2281";
-	private static final Set<String> LEGAL_IDS_SET = Set.of("191111111111", "192222222222");
 	private static final List<String> LEGAL_IDS_LIST = List.of("191111111111", "192222222222");
 	private static final List<String> PARTY_IDS = List.of("28fba79e-73aa-4ecb-939f-301f326d2d4c", "f560865a-51f0-4e96-bca1-55d57a0d3f68");
 
@@ -35,32 +33,6 @@ class PartyIntegrationTest {
 	@AfterEach
 	void verifyInteractions() {
 		verifyNoMoreInteractions(partyClientMock);
-	}
-
-	@Test
-	void getPartyIds_withSet() {
-		final var expectedResult = Map.of(
-			"191111111111", "28fba79e-73aa-4ecb-939f-301f326d2d4c",
-			"192222222222", "f560865a-51f0-4e96-bca1-55d57a0d3f68");
-
-		when(partyClientMock.getPartyIds(MUNICIPALITY_ID, List.copyOf(LEGAL_IDS_SET))).thenReturn(expectedResult);
-
-		final var result = partyIntegration.getPartyIds(MUNICIPALITY_ID, LEGAL_IDS_SET);
-
-		assertThat(result).isEqualTo(expectedResult);
-		verify(partyClientMock).getPartyIds(MUNICIPALITY_ID, List.copyOf(LEGAL_IDS_SET));
-	}
-
-	@Test
-	void getPartyIds_withSet_empty() {
-		final Map<String, String> expectedResult = Map.of();
-
-		when(partyClientMock.getPartyIds(MUNICIPALITY_ID, List.of())).thenReturn(expectedResult);
-
-		final var result = partyIntegration.getPartyIds(MUNICIPALITY_ID, Set.of());
-
-		assertThat(result).isEmpty();
-		verify(partyClientMock).getPartyIds(MUNICIPALITY_ID, List.of());
 	}
 
 	@Test
@@ -86,57 +58,45 @@ class PartyIntegrationTest {
 
 	@Test
 	void getPartyIds_withList_null() {
-		final var result = partyIntegration.getPartyIds(MUNICIPALITY_ID, (List<String>) null);
+		final var result = partyIntegration.getPartyIds(MUNICIPALITY_ID, null);
 
 		assertThat(result).isEmpty();
 	}
 
 	@Test
-	void getPartyIds_clientThrows() {
-		when(partyClientMock.getPartyIds(MUNICIPALITY_ID, List.copyOf(LEGAL_IDS_SET)))
-			.thenThrow(Problem.valueOf(BAD_GATEWAY, "Service unavailable"));
-
-		assertThatThrownBy(() -> partyIntegration.getPartyIds(MUNICIPALITY_ID, LEGAL_IDS_SET))
-			.isInstanceOf(Problem.class)
-			.hasMessageContaining("Bad Gateway");
-
-		verify(partyClientMock).getPartyIds(MUNICIPALITY_ID, List.copyOf(LEGAL_IDS_SET));
-	}
-
-	@Test
-	void getPersonNumbers() {
+	void getLegalIds() {
 		final var expectedResult = Map.of(
 			"28fba79e-73aa-4ecb-939f-301f326d2d4c", "191111111111",
 			"f560865a-51f0-4e96-bca1-55d57a0d3f68", "192222222222");
 
 		when(partyClientMock.getPersonNumbers(MUNICIPALITY_ID, PARTY_IDS)).thenReturn(expectedResult);
 
-		final var result = partyIntegration.getPersonNumbers(MUNICIPALITY_ID, PARTY_IDS);
+		final var result = partyIntegration.getLegalIds(MUNICIPALITY_ID, PARTY_IDS);
 
 		assertThat(result).isEqualTo(expectedResult);
 		verify(partyClientMock).getPersonNumbers(MUNICIPALITY_ID, PARTY_IDS);
 	}
 
 	@Test
-	void getPersonNumbers_empty() {
-		final var result = partyIntegration.getPersonNumbers(MUNICIPALITY_ID, List.of());
+	void getLegalIds_empty() {
+		final var result = partyIntegration.getLegalIds(MUNICIPALITY_ID, List.of());
 
 		assertThat(result).isEmpty();
 	}
 
 	@Test
-	void getPersonNumbers_null() {
-		final var result = partyIntegration.getPersonNumbers(MUNICIPALITY_ID, null);
+	void getLegalIds_null() {
+		final var result = partyIntegration.getLegalIds(MUNICIPALITY_ID, null);
 
 		assertThat(result).isEmpty();
 	}
 
 	@Test
-	void getPersonNumbers_clientThrows() {
+	void getLegalIds_clientThrows() {
 		when(partyClientMock.getPersonNumbers(MUNICIPALITY_ID, PARTY_IDS))
 			.thenThrow(Problem.valueOf(BAD_GATEWAY, "Service unavailable"));
 
-		assertThatThrownBy(() -> partyIntegration.getPersonNumbers(MUNICIPALITY_ID, PARTY_IDS))
+		assertThatThrownBy(() -> partyIntegration.getLegalIds(MUNICIPALITY_ID, PARTY_IDS))
 			.isInstanceOf(Problem.class)
 			.hasMessageContaining("Bad Gateway");
 
