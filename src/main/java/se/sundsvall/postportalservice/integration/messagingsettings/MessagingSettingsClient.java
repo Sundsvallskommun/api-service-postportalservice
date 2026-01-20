56bfae4ee70c2e321285b03f0bd8b3d3f03dfd76
AndreSoderlund <andre.soderlund@b3.se>
2026-01-20 12:19:42 +0100

    Removed unused MessagingSettings resource. Updated MessagingSettings integration to use 3.0

--- src/main/java/se/sundsvall/postportalservice/integration/messagingsettings/MessagingSettingsClient.java
+++ src/main/java/se/sundsvall/postportalservice/integration/messagingsettings/MessagingSettingsClient.java
@@ -4,14 +4,12 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static se.sundsvall.postportalservice.integration.messagingsettings.configuration.MessagingSettingsConfiguration.CLIENT_ID;
 
 import generated.se.sundsvall.messagingsettings.MessagingSettings;
-import generated.se.sundsvall.messagingsettings.SenderInfoResponse;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 import se.sundsvall.dept44.support.Identifier;
 import se.sundsvall.postportalservice.integration.messagingsettings.configuration.MessagingSettingsConfiguration;
 
@@ -22,11 +20,6 @@ import se.sundsvall.postportalservice.integration.messagingsettings.configuratio
 	configuration = MessagingSettingsConfiguration.class,
 	dismiss404 = true)
 public interface MessagingSettingsClient {
-
-	@GetMapping(path = "/{municipalityId}/sender-info", produces = APPLICATION_JSON_VALUE)
-	List<SenderInfoResponse> getSenderInfo(
-		@PathVariable("municipalityId") String municipalityId,
-		@RequestParam("departmentId") String departmentId);
 
 	@GetMapping(path = "/{municipalityId}/user", produces = APPLICATION_JSON_VALUE)
 	List<MessagingSettings> getMessagingSettingsForUser(
