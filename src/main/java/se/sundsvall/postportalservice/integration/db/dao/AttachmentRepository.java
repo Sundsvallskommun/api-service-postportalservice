3c722beb7829aaacf5a877b6e357b57373475c7b
AndreSoderlund <andre.soderlund@b3.se>
2026-06-08 13:13:00 +0200

    Add @CircuitBreaker to all repositories

--- src/main/java/se/sundsvall/postportalservice/integration/db/dao/AttachmentRepository.java
+++ src/main/java/se/sundsvall/postportalservice/integration/db/dao/AttachmentRepository.java
@@ -1,9 +1,11 @@
 package se.sundsvall.postportalservice.integration.db.dao;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import se.sundsvall.postportalservice.integration.db.AttachmentEntity;
 
 @Repository
+@CircuitBreaker(name = "attachmentRepository")
 public interface AttachmentRepository extends JpaRepository<AttachmentEntity, String> {
 }
--- src/main/java/se/sundsvall/postportalservice/integration/db/dao/MessageRepository.java
+++ src/main/java/se/sundsvall/postportalservice/integration/db/dao/MessageRepository.java
@@ -1,5 +1,6 @@
 package se.sundsvall.postportalservice.integration.db.dao;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +10,7 @@ import se.sundsvall.postportalservice.integration.db.MessageEntity;
 import se.sundsvall.postportalservice.integration.db.converter.MessageType;
 
 @Repository
+@CircuitBreaker(name = "messageRepository")
 public interface MessageRepository extends JpaRepository<MessageEntity, String> {
 	Page<MessageEntity> findAllByMunicipalityIdAndUserUsernameIgnoreCase(final String municipalityId, final String username, final Pageable pageable);
 
